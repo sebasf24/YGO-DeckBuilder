91a6639673eb905cd808b0416e053d9ce31ce56a
sebasf24 <sebastianferreyra@gmail.com>
2020-11-05 19:08:22 -0300

    test3

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './App.css';
 import {Provider} from 'react-redux';
-import store from './src/store';
-import Search from './src/components/Search/Search';
-import CardContainer from './src/components/Container/Container.js';
+import store from './store';
+import Search from './components/Search/Search';
+import CardContainer from './components/Container/Container.js';
 
 function App() {
-  console.log(document.getElementById('root'))
+  //console.log(document.getElementById('root'))
   return (
     <Provider store={store}>
       <Search></Search>
