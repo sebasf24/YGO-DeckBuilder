0a1ce1c27c233f70ac344cb716bf754b1c279ece
sebasf24 <sebastianferreyra@gmail.com>
2020-11-10 19:33:30 -0300

    build

---
+++ build/precache-manifest.dc347e994937e43b12063ba8051d8000.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a66f0cecddedebce629e5af2b46fe23",
+    "url": "/YGO-DeckBuilder/index.html"
+  },
+  {
+    "revision": "06a58330283161a5f751",
+    "url": "/YGO-DeckBuilder/static/css/main.fd71a97e.chunk.css"
+  },
+  {
+    "revision": "c7731d1e622089e80282",
+    "url": "/YGO-DeckBuilder/static/js/2.ea8ec556.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/YGO-DeckBuilder/static/js/2.ea8ec556.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06a58330283161a5f751",
+    "url": "/YGO-DeckBuilder/static/js/main.6516a8f7.chunk.js"
+  },
+  {
+    "revision": "512b3aef4410148adbda",
+    "url": "/YGO-DeckBuilder/static/js/runtime-main.87c27ed0.js"
+  }
+]);
